a980208b7e2ec152b4a44b6746a776c2f0ac9f65
BigBabyofTel <augustus.tb@outlook.com>
2025-12-01 21:26:57 -0600

    login page created

---
+++ frontend/app/portal/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+
+    return (
+        <>
+            <div
+                className="h-dvh w-full flex flex-row items-center justify-around">
+                <div className="w-1/4 h-1/5 p-5  bg-gray-200 text-center">Staff</div>
+                <div className="w-1/4 h-1/5 p-5  bg-gray-200 text-center">Student</div>
+                <div className="w-1/4 h-1/5 p-5  bg-gray-200 text-center">Parent</div>
+            </div>
+        </>
+    )
+}
