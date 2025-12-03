8ebcc33b4ae30711eeb655e696995421a201969f
BigBabyofTel <augustus.tb@outlook.com>
2025-12-02 23:54:57 -0600

    login screen images added

--- frontend/app/portal/page.tsx
+++ frontend/app/portal/page.tsx
@@ -1,12 +1,19 @@
+import Image from "next/image"
+
 export default function Page() {
 
     return (
         <>
             <div
                 className="h-dvh w-full flex flex-row items-center justify-around">
-                <div className="w-1/4 h-1/5 p-5  bg-gray-200 text-center">Staff</div>
-                <div className="w-1/4 h-1/5 p-5  bg-gray-200 text-center">Student</div>
-                <div className="w-1/4 h-1/5 p-5  bg-gray-200 text-center">Parent</div>
+                <div className="w-1/4 h-1/5 p-5  bg-gray-200 text-center">Staff
+                    <Image src="/teacher.webp" width={50} height={50} alt="an icon representing teachers"/></div>
+                <div className="w-1/4 h-1/5 p-5  bg-gray-200 text-center">Student
+                    <Image src="/student.webp" width={50} height={50} alt="an icon representing students"/>
+                </div>
+                <div className="w-1/4 h-1/5 p-5  bg-gray-200 text-center">Parent
+                    <Image src="/parent.webp" width={50} height={50} alt="icon representing parents"/>
+                </div>
             </div>
         </>
     )
