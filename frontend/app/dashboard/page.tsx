78d2dde37d3fb3154287733526d0d9cca9b2b899
BigBabyofTel <augustus.tb@outlook.com>
2025-12-04 23:08:27 -0600

    button added to staff card on login page

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+
+    return (
+        <>
+            <h1>
+                This is the dashboard
+            </h1>
+            <span>
+                this is where you should be able to create a group and see all existing groups
+            </span>
+        </>
+    )
+}
--- frontend/app/portal/page.tsx
+++ frontend/app/portal/page.tsx
@@ -1,13 +1,15 @@
+"use client"
 import Image from "next/image"
+import {useRouter} from "next/navigation"
 
 export default function Page() {
-
+    const router = useRouter()
     return (
         <>
             <section
                 className="h-dvh w-full flex flex-col items-center justify-evenly bg-linear-to-r from-yellow-400 to-orange-300">
                 <div>
-                    <h1 className="text-5xl">LuMineScence</h1>
+                    <h1 className="text-5xl dark:text-white">LuMineScence</h1>
                 </div>
                 <div>
                     <h2 className="text-lg">Sign in to your account as</h2>
@@ -15,6 +17,8 @@ export default function Page() {
                 <div
                     className="w-full flex flex-row items-center justify-around">
                     <div
+                        role="button"
+                        onClick={() => router.push('/dashboard')}
                         className="ww-1/4 h-full bg-white/20 backdrop-blur-md border border-white/30 rounded-lg shadow-lg text-center flex flex-col justify-between items-center opacity-100">
                         <span className="mt-5">Staff</span>
                         <Image src="/teacher.webp" width={100} height={100} alt="an icon representing teachers"/></div>
