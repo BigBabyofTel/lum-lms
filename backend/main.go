31b5e5ea623b8b47cee8154d7237918942c3e400
BigBabyofTel <augustus.tb@outlook.com>
2025-12-12 13:07:45 -0600

    first route with gin

---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/home", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	router.Run()
+
+}
