afe29053a80d827620002822af89b07dbf9b812a
BigBabyofTel <augustus.tb@outlook.com>
2026-01-11 12:35:40 -0600

    button added to landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,14 @@
 export default function Home() {
     return (
         <>
-            <div>
+            <div className="border p-2">
                 <h1 className="text-6xl font-bold">Welcome to Luminescence</h1>
                 <h2>This is the landing page!!</h2>
+                <button className="border p-2 rounded-3xl bg-slate-400 hover:bg-slate-500 text-white">
+                    <a href="http://localhost:3000/portal" className="text-xl">
+                        Enter Portal
+                    </a>
+                </button>
             </div>
         </>
     );
