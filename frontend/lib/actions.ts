37371436eec54900bbf49146082634c3e5b594cd
BigBabyofTel <augustus.tb@outlook.com>
2026-03-25 22:42:14 -0500

    user store updated

--- frontend/lib/actions.ts
+++ frontend/lib/actions.ts
@@ -1,11 +1,11 @@
 "use server"
 
-import {Class, FormState} from "@/lib/types";
+import {Class, FormState, User} from "@/lib/types";
 import {redirect} from "next/navigation"
-import {testUserSchema} from "@/lib/schemas";
 import * as z from "zod";
+import {testUserSchema} from "@/lib/schemas";
 
-export async function submitForm(state: FormState | null, formData: FormData): Promise<FormState> {
+export async function submitForm(formData: FormData): Promise<FormState> {
     const subject = formData.get('subject')
     const grade = formData.get('grade')
     const teacher_id = formData.get('teacherId')
@@ -42,7 +42,6 @@ export async function getAllClasses(teacherId: string): Promise<Class[]> {
         console.error("Teacher Id is missing")
     }
     const valid = testUserSchema.safeParse({id: teacherId})
-
     if (!valid.success) {
         console.error("Id can not be validated")
     }
@@ -54,6 +53,25 @@ export async function getAllClasses(teacherId: string): Promise<Class[]> {
         })
         if (!response.ok) return []
         return await response.json() as Class[]
+    } catch (err) {
+        if (err instanceof z.ZodError) {
+            console.error(err.issues)
+        } else {
+            console.error(err)
+        }
+    }
+    return []
+}
+
+
+export async function getAllStudents(): Promise<User[]> {
+    try {
+        const response = await fetch(`http://localhost:8080/api/v1/user`, {
+            method: 'GET',
+            headers: {'Accept': 'application/json'},
+        })
+        if (!response.ok) return []
+        return await response.json() as User[]
     } catch (err) {
         if (err instanceof z.ZodError) {
             console.error(err.issues)
